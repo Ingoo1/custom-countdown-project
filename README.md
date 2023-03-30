dc25e7892dec3939e2bbd0af5209ff81c27efec7
Maksym_enKO <ingool.ua@gmail.com>
2023-03-30 16:38:37 +0300

    form values

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # custom-countdown-project
 
-## [Priview Link]()
+## [Preview Link](https://ingoo1.github.io/custom-countdown-project/)
 
 Used links:
 
 1. [Pixabay](https://pixabay.com/videos/)
 2. [Free Online File Compressor](https://www.youcompress.com/)
-3. []()
-4. []()
+3. [ARIA](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA)
+4. [<input type="date">](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/date)
+5. [JavaScript Date Reference](https://www.w3schools.com/jsref/jsref_obj_date.asp)
+6. [Date.prototype.toISOString()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString)
+7. [JavaScript String split()](https://www.w3schools.com/jsref/jsref_split.asp)
+8. [HTMLFormElement: submit event](https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event)
+9. [preventDefault() Event Method](https://www.w3schools.com/jsref/event_preventdefault.asp)
+10. []()
+11. []()
---
+++ script.js
@@ -0,0 +1,35 @@
+const countdownForm = document.getElementById('countdownForm');
+const inputContainer = document.getElementById('input-container');
+const dateEl = document.getElementById('date-picker');
+
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
+
+const completeEl = document.getElementById('complete');
+const completeElInfo = document.getElementById('complete-info');
+const completeBtn = document.getElementById('complete-button');
+
+let countdownTitle = '';
+let countdownDate = '';
+let countdownValue = Date;
+let countdownActive;
+let savedCountdown;
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+const today = new Date().toISOString().split('T')[0];
+dateEl.setAttribute('min', today);
+
+function updateCountdown(e) {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+  console.log(countdownTitle, countdownDate);
+}
+
+countdownForm.addEventListener('submit', updateCountdown);
